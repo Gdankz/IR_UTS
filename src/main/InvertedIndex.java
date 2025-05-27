5d28391cddd9e9a2ce0c560dbf9d65ce76e6f1a2
Nicolaus Reva <nicolaussagraha14@gmail.com>
2025-05-27 18:24:19 +0700

    fix dah selesai

--- src/main/InvertedIndex.java
+++ src/main/InvertedIndex.java
@@ -15,58 +15,56 @@ public class InvertedIndex {
     private LinkedListOrdered<Term> dictionary = new LinkedListOrdered<>();
 
     // bikin proses dictionary dan postinglist
-//    private void makeInvertedIndex() {
-//
-//        File folder = new File(PATH);
-//        if (!folder.exists() || !folder.isDirectory()) {
-//            System.out.println("Folder tidak ada");
-//            return;
-//        }
-//
-//
-//        for (File file : folder.listFiles()) {
-//            if (file.isFile() && file.getName().startsWith("doc") && file.getName().endsWith(".txt")) {
-//                int docId = extractDocId(file.getName());
-//
-//                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-//                    String line;
-//                    while ((line = br.readLine()) != null) {
-//
-//                        String[] words = line.toLowerCase().split("\\W+");
-//
-//                        for (String word : words) {
-//                            if (word.isEmpty()) continue;
-//
-//                            word = word.replaceAll(willIgnore, "");
-//                            String stemmed = Stemmer.stem(word);
-//
-//                            if (stemmed.isEmpty()) continue;
-//
-//                            Term existingTerm = findTermInDictionary(stemmed);
-//
-//                            if (existingTerm == null){
-//                                Term newTerm = new Term(stemmed,1);
-//                                String docName = "doc" + docId;
-//                                Document newDoc = new Document(docName, 1);
-//                                newTerm.setDocs(newDoc);
-//                                dictionary.addSort(newTerm);
-//                            } else {
-//                                if (!containsDoc(existingTerm, docId)){
-//                                    existingTerm.setDF(existingTerm.getDF() + 1);
-//                                }
-//
-//                                addDocumentToTerm(existingTerm, docId);
-//                            }
-//                        }
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//        System.out.println("Dictionary berhasil dibuat dengan " + dictionary.size() + " term.");
-//    }
+    private void makeInvertedIndex() {
 
+        File folder = new File(PATH);
+        if (!folder.exists() || !folder.isDirectory()) {
+            System.out.println("Folder tidak ada");
+            return;
+        }
+
+        for (File file : folder.listFiles()) {
+            if (file.isFile() && file.getName().startsWith("doc") && file.getName().endsWith(".txt")) {
+                int docId = extractDocId(file.getName());
+
+                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+
+                        String[] words = line.toLowerCase().split("\\W+");
+
+                        for (String word : words) {
+                            if (word.isEmpty()) continue;
+
+                            word = word.replaceAll(willIgnore, "");
+                            String stemmed = Stemmer.stem(word);
+
+                            if (stemmed.isEmpty()) continue;
+
+                            Term existingTerm = findTermInDictionary(stemmed);
+
+                            if (existingTerm == null){
+                                Term newTerm = new Term(stemmed,1);
+                                String docName = "doc" + docId;
+                                Document newDoc = new Document(docName, 1);
+                                newTerm.setDocs(newDoc);
+                                dictionary.addSort(newTerm);
+                            } else {
+                                if (!containsDoc(existingTerm, docId)){
+                                    existingTerm.setDF(existingTerm.getDF() + 1);
+                                }
+
+                                addDocumentToTerm(existingTerm, docId);
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("Dictionary berhasil dibuat dengan " + dictionary.size() + " term.");
+    }
 
     private int extractDocId(String fileName){
         String numberOnly = fileName.replaceAll("[^0-9]", "");
@@ -132,6 +130,7 @@ public class InvertedIndex {
 
         for (int i = 1; i < selectedTerms.size(); i++) {
             LinkedListOrdered<Document> docs = selectedTerms.get(i).getDocs();
+
             result = intersect(result, docs);
         }
 
@@ -155,10 +154,6 @@ public class InvertedIndex {
     // bikin fungsi buat ngebaca query terus output postinglist dari query itu
     private LinkedListOrdered<Term> getSelected(String query) {
         String[] terms = query.toLowerCase().split("\\W+");
-
-        for (String term : terms){
-            System.out.println(term);
-        }
 
         LinkedListOrdered<Term> f = new LinkedListOrdered<>();
 
@@ -265,165 +260,184 @@ public class InvertedIndex {
     }
 
 
-    private void makeInvertedIndex() {
-        File folder = new File(PATH);
-        makeMaster();
+//    private void makeInvertedIndex() {
+//        File folder = new File(PATH);
+//        makeMaster();
+//
+//        if (!folder.exists() || !folder.isDirectory()) {
+//            System.out.println("Folder tidak ada");
+//            return;
+//        }
+//
+//        for (File file : folder.listFiles()) {
+//            if (file.isFile() && file.getName().startsWith("doc") && file.getName().endsWith(".txt")) {
+//                dictionary = partInvertedIndex(dictionary, file);
+//            }
+//        }
+//    }
 
-        if (!folder.exists() || !folder.isDirectory()) {
-            System.out.println("Folder tidak ada");
-            return;
-        }
+//    public LinkedListOrdered<Term> partInvertedIndex(LinkedListOrdered<Term> dictionary, File file) {
+//        int docId = extractDocId(file.getName());
+//        String docName = "doc" + docId;
+//        LinkedListOrdered<Term> selectedTerms = new LinkedListOrdered<>(); // Ini barusan ditambah
+//
+//        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+//            String line;
+//            while((line = br.readLine()) != null) {
+//                String[] words = line.toLowerCase().split("\\W+");
+//                for (String word : words) {
+//                    if (word.isEmpty()) continue;
+//
+//                    word = word.replaceAll(willIgnore, "");
+//                    String stemmed = Stemmer.stem(word);
+//
+//                    Term existingTerm = findTermInDictionary(stemmed);
+//
+//                    if (stemmed.equals("semarang")) {
+//                        System.out.println(stemmed);
+//                    }
+//
+//                    if(existingTerm == null) {
+//                        Term newTerm = new Term(stemmed);
+//                        Document newDoc = new Document(docName, 1);
+//                        newTerm.setDocs(newDoc);
+//                        newTerm.setDF(1);
+//                        dictionary.addSort(newTerm);
+//                        selectedTerms.addSort(newTerm);
+//
+//                        if (stemmed.equals("semarang")) {
+//                            if (dictionary.contains(newTerm)) {
+//                                System.out.println("kata semarang ada");
+//                            }
+//                        }
+//
+//                    } else {
+//
+//                        if (existingTerm.getTerm().equals("semarang")) {
+//                            System.out.println(existingTerm.getTerm() + " sudah ada");
+//                        }
+//
+//                        LinkedListOrdered<Document> docs = existingTerm.getDocs();
+//                        boolean docExists = false;
+//
+//                        ListIterator<Document> docIt = docs.listIterator();
+//                        while (docIt.hasNext()) {
+//                            Document doc = docIt.next();
+//                            if (doc.getName().equals(docName)) {
+//                                doc.setTF(doc.getTF() + 1);
+//                                docExists = true;
+//                                break;
+//                            }
+//                        }
+//
+//                        if (!docExists) {
+//                            Document newDoc = new Document(docName, 1);
+//                            docs.addSort(newDoc);
+//                            existingTerm.setDF(existingTerm.getDF() + 1);
+//
+//                        }
+//
+//                        boolean alreadyAdded = false;
+//                        ListIterator<Term> termIt = selectedTerms.listIterator();
+//                        while (termIt.hasNext()) {
+//                            if (termIt.next().getTerm().equals(stemmed)) {
+//                                alreadyAdded = true;
+//                                break;
+//                            }
+//                        }
+//
+//                        if (!alreadyAdded) {
+//                            selectedTerms.addSort(existingTerm);
+//                        }
+//                    }
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        return selectedTerms;
+//    }
 
-        for (File file : folder.listFiles()) {
-            if (file.isFile() && file.getName().startsWith("doc") && file.getName().endsWith(".txt")) {
-                dictionary = partInvertedIndex(dictionary, file);
-            }
-        }
-    }
-
-    public LinkedListOrdered<Term> partInvertedIndex(LinkedListOrdered<Term> dictionary, File file) {
-        int docId = extractDocId(file.getName());
-        String docName = "doc" + docId;
-        LinkedListOrdered<Term> selectedTerms = new LinkedListOrdered<>(); // Ini barusan ditambah
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while((line = br.readLine()) != null) {
-                String[] words = line.toLowerCase().split("\\W+");
-
-                for (String word : words) {
-                    if (word.isEmpty()) continue;
-
-                    word = word.replaceAll(willIgnore, "");
-                    String stemmed = Stemmer.stem(word);
-
-                    Term existingTerm = findTermInDictionary(stemmed);
-
-                    if (stemmed.isEmpty()) continue;
-
-                    if(existingTerm == null) {
-                        Term newTerm = new Term(stemmed);
-                        Document newDoc = new Document(docName, 1);
-                        newTerm.setDocs(newDoc);
-                        newTerm.setDF(1);
-                        dictionary.addSort(newTerm);
-                        selectedTerms.addSort(newTerm);
-                } else {
-                        LinkedListOrdered<Document> docs = existingTerm.getDocs();
-                        boolean docExists = false;
-
-                        ListIterator<Document> docIt = docs.listIterator();
-                        while (docIt.hasNext()) {
-                            Document doc = docIt.next();
-                            if (doc.getName().equals(docName)) {
-                                doc.setTF(doc.getTF() + 1);
-                                docExists = true;
-                                break;
-                            }
-                        }
-
-                        if (!docExists) {
-                            Document newDoc = new Document(docName, 1);
-                            docs.addSort(newDoc);
-                            existingTerm.setDF(existingTerm.getDF() + 1);
-                            
-                        }
-
-                        boolean alreadyAdded = false;
-                        ListIterator<Term> termIt = selectedTerms.listIterator();
-                        while (termIt.hasNext()) {
-                            if (termIt.next().getTerm().equals(stemmed)) {
-                                alreadyAdded = true;
-                                break;
-                            }
-                        }
-                        if (!alreadyAdded) {
-                            selectedTerms.addSort(existingTerm);
-                        }
-
-                    // Term existingTerm = findTermInDictionary(stemmed);
-
-                    // if (existingTerm == null){
-                    //     Term newTerm = new Term(stemmed,1);
-                    //     String docName = "doc" + docId;
-                    //     Document newDoc = new Document(docName, 1);
-                    //     newTerm.setDocs(newDoc);
-                    //     dictionary.addSort(newTerm);
-                    //     selectedTerms.addSort(newTerm);
-                    // } else {
-                    //     if (containsDoc(existingTerm, docId)) {
-                    //         existingTerm.setDF(existingTerm.getDF() + 1);
-                    //     }
-                        
-                    //     String docName = file.getName().split("\\W+")[0];
-
-                    //     for (Document doc : existingTerm.getDocs()) {
-                    //         if (doc.getName().equals(docName)){
-                    //             doc.setTF(doc.getTF() + 1);
-                    //             existingTerm.setDF(existingTerm.getDF() + 1);
-                    //         }
-                    //     }
-                    //     addDocumentToTerm(existingTerm, docId);
-                    //     selectedTerms.addSort(existingTerm);
-                    // }
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return selectedTerms;
-    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
         InvertedIndex index = new InvertedIndex();
-        index.makeInvertedIndex();
-        boolean loop = true;
+        Scanner sc = new Scanner(System.in);
 
+        Runtime runtime = Runtime.getRuntime();
+
+        runtime.gc();
+
+        long memoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Memori terpakai SEBELUM membuat index: " + toReadableFormat(memoryBefore));
+
+        long startTime = System.nanoTime();
+        index.makeInvertedIndex();
+
+        long endTime = System.nanoTime();
+
+        runtime.gc();
+
+        long memoryAfter = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Memori terpakai SETELAH membuat index: " + toReadableFormat(memoryAfter));
+
+        long memoryUsed = memoryAfter - memoryBefore;
+        long timeTaken = endTime - startTime;
+
+        System.out.println("\n================ HASIL ANALISIS ===============");
+        System.out.println("Memori yang digunakan untuk proses indexing: " + toReadableFormat(memoryUsed));
+        System.out.println("Waktu yang dibutuhkan untuk proses indexing: " + (timeTaken / 1_000_000) + " ms");
+        System.out.println("============================================\n");
+
+        boolean loop = true;
         while (loop) {
             show();
             System.out.print("Pilih: ");
             int choice = sc.nextInt();
+            sc.nextLine(); // Membersihkan sisa newline
 
             if (choice == 1) {
                 System.out.print("Masukkan term: ");
-                String term = sc2.nextLine();
+                String term = sc.nextLine();
+
+                runtime.gc();
+                long searchMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
+                long searchTimeStart = System.nanoTime();
+
+                // Jalankan proses searching
                 index.search(term);
 
+                long searchTimeEnd = System.nanoTime();
+                runtime.gc();
+                long searchMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
+
+                System.out.println("\n--- Analisis Proses Pencarian ---");
+                System.out.println("Memori yang digunakan untuk search: " + toReadableFormat(searchMemoryAfter - searchMemoryBefore));
+                System.out.println("Waktu yang dibutuhkan untuk search: " + ((searchTimeEnd - searchTimeStart) / 1_000_000.0) + " ms");
+                System.out.println("---------------------------------");
+
             } else if (choice == 2) {
-                boolean thiswillLoop = true;
-                showList();
-                System.out.print("Pilih: ");
-                while(thiswillLoop) {
-
-
-                    int pilihan = sc.nextInt();
-
-                    if (pilihan <= masterDocs.size()) {
-                        index.selectedFile(masterDocs.get(pilihan-1));
-                        thiswillLoop = false;
-                    } else if (pilihan == 0) {
-                        thiswillLoop = false;
-                    } else if (pilihan == 99) {
-                        index.showDics();
-                    } else {
-                        System.out.println("Pilihan tidak valid");
-                        System.out.print("Pilih: ");
-                    }
-                }
-
+                index.showDics();
             } else if (choice == 3) {
                 loop = false;
             }
-
             System.out.println("");
         }
+        sc.close();
     }
+
+    /**
+     * Helper method untuk mengubah byte menjadi format yang mudah dibaca (KB/MB).
+     */
+    public static String toReadableFormat(long bytes) {
+        if (bytes < 1024) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(1024));
+        String pre = "KMGTPE".charAt(exp-1) + "";
+        return String.format("%.2f %sB", bytes / Math.pow(1024, exp), pre);
+    }
+
 
     static void showList() {
         int i = 0;
